75b0e65fdc21cbd783c402df57039a8bc748dc4d
sanayuj <sanayuj2255@gmail.com>
2024-02-08 18:38:07 +0530

    first commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+var greeting = "Hello, playground"
+print(greeting)
+
+
+//var: Use var to declare mutable variables, which means their values can be changed after initialization.
+var Num=10
+Num=23
+//let: Use let to declare immutable constants, which means their values cannot be changed after initialization.
+let Data=34
+
+print(Data)
+print(Num)
+
+//variable declared with specify the datatype
+var myInteger: Int = 10
